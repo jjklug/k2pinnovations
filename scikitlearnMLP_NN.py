1e677ea9471c5b4b4edf8cd71200802eae85fb99
David-Poulin <dpoulin@uvm.edu>
2025-04-24 13:28:38 -0400

    Graph Compete

--- scikitlearnMLP_NN.py
+++ scikitlearnMLP_NN.py
@@ -1,6 +1,8 @@
+import matplotlib.pyplot as plt
 from sklearn.neural_network import MLPClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.datasets import make_classification
+
 
 from sklearn.metrics import accuracy_score
 
@@ -9,19 +11,25 @@ import numpy as np
 
 data = pd.read_csv('mas_data_ex_3.5.csv')
 
-X = data[['x1', 'x2']].values
+X = data[['x1', 'x2']].values #input states
 
-y = data['inside_MAS'].values
+y = data['inside_MAS'].values  # whether inside or outside of MAS
 
-#print(X)
-#print(y)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3) #70/30 train test split
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
-
-clf = MLPClassifier(hidden_layer_sizes=(100,), max_iter=300, random_state=42)
+#defaults uses relu which can handle nonlinearity
+#MPLClassifier is used for binary
+clf = MLPClassifier(hidden_layer_sizes=(20,10), max_iter=300, random_state=42)
+#clf = MLPClassifier(hidden_layer_sizes=(20, 20), max_iter=300, random_state=42)
 clf.fit(X_train, y_train)
 
-predictions = clf.predict(X_test)
+predictions = clf.predict(X_test) #predictions
 
+print("Accuracy:", accuracy_score(y_test, predictions, normalize=True)) #show accuracy
 
-print("Accuracy:", accuracy_score(y_test, predictions, normalize=True))
+#graph
+all_prediction = clf.predict(X)
+x = X[:, 0]
+y = X[:, 1]
+plt.scatter(x, y, c=all_prediction, cmap='viridis', s=100) 
+plt.show()
